2429f1c8352cfed0f99dc0e10c1064b85e8af575
yuqiha <522116296@qq.com>
2026-04-22 16:22:11 +0800

    tsel pto-isa 的 pintlv_b16 接受 (b32, b16)，但 tilelang-dsl 要求 (b16, b16)，不支持

--- lib/TileOps/tsel_template.py
+++ lib/TileOps/tsel_template.py
@@ -8,7 +8,7 @@ Mask tile format:
 - For f32: mask uses ui8 (packed predicate, 1 byte per lane)
 - For f16/i8: mask uses ui32 (packed predicate, 32-bit aligned)
 
-REQUIRES: tilelang_dsl support for plds, pintlv_b16 operations
+REQUIRES: tilelang_dsl support for plds, pintlv_b16, castptr operations
 """
 
 import sys
@@ -20,9 +20,9 @@ import tilelang_dsl as pto
     target="a5",
     op="pto.tsel",
     dtypes=[
-        (pto.f32, pto.ui8, pto.f32, pto.f32, pto.f32, pto.f32),
-        (pto.f16, pto.ui32, pto.f16, pto.f16, pto.f16, pto.f16),
-        (pto.i8, pto.ui32, pto.i8, pto.i8, pto.i8, pto.i8),
+        (pto.f32, pto.f32, pto.f32, pto.f32, pto.f32),
+        (pto.f16, pto.f16, pto.f16, pto.f16, pto.f16),
+        (pto.i8, pto.i8, pto.i8, pto.i8, pto.i8),
     ],
     advanced=True
 )
@@ -31,26 +31,33 @@ def template_tsel(mask: pto.Tile, src0: pto.Tile, src1: pto.Tile, tmp: pto.Tile,
     valid_rows, valid_cols = dst.valid_shape
 
     lanes = pto.get_lanes(dtype)
+    
+    mask_base_ptr = mask.as_ptr()
+    
+    if pto.constexpr(dtype == pto.f32):
+        mask_ptr = pto.castptr(mask_base_ptr, pto.ptr(pto.ui8, pto.MemorySpace.UB))
+    else:
+        mask_ptr = pto.castptr(mask_base_ptr, pto.ptr(pto.ui32, pto.MemorySpace.UB))
 
     for row in range(0, valid_rows, 1):
         remained = valid_cols
         for col in range(0, valid_cols, lanes):
             pred_mask, remained = pto.make_mask(dtype, remained)
             if pto.constexpr(dtype == pto.f32):
-                select_mask = pto.plds(mask[row, col:].as_ptr(pto.ui8, pto.MemorySpace.UB), 0, "US")
+                select_mask = pto.plds(mask_ptr, row * valid_cols + col, pto.PredicateDist.US)
                 select_mask0, select_mask1 = pto.pintlv_b16(select_mask, pto.pset_b16(pto.MaskPattern.ALL))
                 lhs = pto.vlds(src0[row, col:])
                 rhs = pto.vlds(src1[row, col:])
                 selected = pto.vsel(lhs, rhs, select_mask0)
                 pto.vsts(selected, dst[row, col:], pred_mask)
             elif pto.constexpr(dtype == pto.f16):
-                select_mask = pto.plds(mask[row, col:].as_ptr(pto.ui32, pto.MemorySpace.UB), 0, "US")
+                select_mask = pto.plds(mask_ptr, row * valid_cols + col, pto.PredicateDist.US)
                 lhs = pto.vlds(src0[row, col:])
                 rhs = pto.vlds(src1[row, col:])
                 selected = pto.vsel(lhs, rhs, select_mask)
                 pto.vsts(selected, dst[row, col:], pred_mask)
             else:
-                select_mask = pto.plds(mask[row, col:].as_ptr(pto.ui32, pto.MemorySpace.UB), 0, "US")
+                select_mask = pto.plds(mask_ptr, row * valid_cols + col, pto.PredicateDist.NORM)
                 lhs = pto.vlds(src0[row, col:])
                 rhs = pto.vlds(src1[row, col:])
                 selected = pto.vsel(lhs, rhs, select_mask)
